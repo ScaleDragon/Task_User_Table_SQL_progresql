034ff34913310967bedaf807081e1b692a59e5ff
Vyacheslav L. <Dwek18@mail.ru>
2024-11-07 19:25:00 +0300

    add Script for SQL

---
+++ src/main/java/ru/netology/Script-1.sql
@@ -0,0 +1,21 @@
+create schema netology
+
+    create table netology.PERSONS
+    (
+        name           varchar not null,
+        surname        varchar not null,
+        age            int     not null,
+        phone_number   varchar not null,
+        city_of_living varchar not null,
+        PRIMARY KEY (name, surname, age)
+    );
+
+
+insert into netology.PERSONS
+    (name, surname, age, phone_number, city_of_living)
+values ('Ivan', 'Ivanov', '23', '78032138321', 'Moscow'),
+       ('Oleg', 'Romanov', '43', '7903452131', 'Novosibirsk'),
+       ('Olya', 'Sky', '25', '78053428765', 'Tula'),
+       ('Dmitri', 'Dmitriev', '54', '78213209234', 'Moscow'),
+       ('Anastasiya', 'Air', '65', '79153478907', 'Sochi');
+
